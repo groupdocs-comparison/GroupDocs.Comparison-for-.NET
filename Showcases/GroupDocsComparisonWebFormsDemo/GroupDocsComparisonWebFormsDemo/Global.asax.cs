c9a31a4f46474780eafa78bbfb5267005bca3576
Muhammad Rizwan Niazi <muhammad.rizwan@groupdocs.com>
2016-03-21 01:07:21 +0500

    Added - Showcase MVC Web API Example using GroupDocs.Comparison for .NET API

---
+++ Showcases/GroupDocsComparisonWebFormsDemo/GroupDocsComparisonWebFormsDemo/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Http;
+
+namespace GroupDocsComparisonWebFormsDemo
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{action}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
